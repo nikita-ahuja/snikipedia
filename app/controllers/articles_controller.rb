2677b2879bf98c70df66f299479365db9cbb40a6
Vincent Lu <vincentlu@Vincents-MacBook-Pro.local>
2017-11-01 14:17:12 -0500

    more features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,26 @@
 class ArticlesController < ApplicationController
 
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+
+		if @article.save
+			redirect_to article_path
+		else
+			render "new"
+		end
+	end
+
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :body, :summary, :user_id, :category_id)
+		end
 
 end
