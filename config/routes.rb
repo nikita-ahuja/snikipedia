a17eca6fe149d16ba9971876d9db71948da1865d
Nikita Ahuja <email": null>
2017-11-01 14:33:22 -0500

    Add custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :photos, only: [:new, :create, :index, :destroy]
   root to: 'photos#index'
+  resources :users, :sessions
 
+ get ‘signup’, to: "users#new", as: ‘signup’
+ get ‘login’, to: "sessions#new", as: ‘login’
+ get ‘logout’, to: "sessions#destroy", as: ‘logout’
 
 
   get 'home/index'
